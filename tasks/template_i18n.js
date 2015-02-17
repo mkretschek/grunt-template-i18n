cd68ac839749e01fef7da337cc64c13301fad818
Mathias Kretschek <mathias@kretschek.com.br>
2015-02-17 20:50:14 -0200

    refactor(general): adjust the initial code

--- tasks/template_i18n.js
+++ tasks/template_i18n.js
@@ -17,25 +17,21 @@ module.exports = function(grunt) {
   var Processor = require('../lib/processor');
   var util = require('../lib/util');
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('template_i18n', 'Easy template internationalization.', function() {
     var options = this.options({
-      localeSrc: 'i18n/**/*.yml',
-      pattern: null,
-      parser: null,
+      localeSrc: null,
+      pattern: Processor.DEFAULT_PATTERN,
+      parser: Processor.DEFAULT_PARSER,
       processor: 'dot',
       pluralization: null,
       interpolation: null
     });
 
-
     var locale = Locale.loadSources(options.localeSrc);
     var processor = new Processor(locale, options);
 
     this.files.forEach(function (file) {
-      if (!grunt.file.isDir(file.dest) && file.src.length > 1) {
+      if (!util.isDir(file.dest) && file.src.length > 1) {
         grunt.fail.warn(
           'Unable to process multiple source files into a single destination. ' +
           'Make sure your destination is a folder or that you have a single ' +
@@ -46,14 +42,10 @@ module.exports = function(grunt) {
       var sources = util.removeInvalidFiles(file.src);
       grunt.verbose.writeln(sources.length + ' source(s) found');
 
-      if (!sources.length) {
-        grunt.fail.warn('Unable to find any source file.');
-      }
-
       sources.forEach(function (src) {
         grunt.verbose.write('Processing ' + src + '...');
 
-        var dest = grunt.file.isDir(file.dest) ?
+        var dest = util.isDir(file.dest) ?
           path.join(file.dest, src) :
           file.dest;
 
